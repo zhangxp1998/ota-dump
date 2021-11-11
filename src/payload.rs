8d8eea15b3e87d142be026bdffa94bf0d5ba9a7d
Kelvin Zhang <zhangkelvin@google.com>
2021-11-10 19:37:59 -0800

    Print hashes in hex digest format

--- src/payload.rs
+++ src/payload.rs
@@ -2,6 +2,26 @@ use binread::BinRead;
 use prost::{DecodeError, Message};
 
 pub mod update_engine {
+
+    pub mod payload_formatter {
+        use serde::{Deserializer, Serializer};
+
+        pub fn serialize<S>(data: &Option<Vec<u8>>, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            match data {
+                Some(data) => serializer.serialize_str(hex::encode_upper(data).as_str()),
+                None => serializer.serialize_str("\"\""),
+            }
+        }
+        pub fn deserialize<'a, D>(_: D) -> Result<Option<Vec<u8>>, D::Error>
+        where
+            D: Deserializer<'a>,
+        {
+            todo!("Deserializing Vec<u8> from hex string is not implemented")
+        }
+    }
     include!("../target/protobuf_out/chromeos_update_engine.rs");
 }
 
